2f2ce232e026d63f5be54c341b574e68ceca12ec
kelgreen <raqguina@hotmail.com>
2018-07-02 15:43:05 -0400

    add code javascript pour la page pendu

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+
 "use strict";
 let mots =["javascript","react","angular","bootstrap","html","css"];
 let quantite = mots.length-1;
@@ -13,11 +14,22 @@ let bonchoix = false;
 let enjouant = false;
 let player;
 
-function lettredefini(l) {
+document.getElementById("btnnouveaujeu").addEventListener("click",debut);
+document.getElementById("consignes").addEventListener("click",consignes);
+document.getElementById("jouer").addEventListener("click",jouer);
 
+function lettredefini(l) {
+    let obj;
+    for (let i=0;i<16;i++){
+        obj = document.getElementById("lettre"+i).value="";
+        obj = document.getElementById("lettre"+i).style.display="none";
+    }
+    for(let i=0;i<l;i++){
+        obj = document.getElementById("lettre"+i).style.display="inline-block";
+    }
 }
 
-function debut() {
+function debut(evt) {
     enjouant = true;
     player = document.getElementById("joueurlettreid");
     player.value ="";
@@ -30,4 +42,25 @@ function debut() {
     motchoisi = mots[pos];
     mottaille = motchoisi.length;
     lettredefini = (mottaille);
+    document.getElementById("msg").innerHTML="";
+    partiesimg[1]=document.getElementById("tete");
+    partiesimg[2]=document.getElementById("corps");
+    partiesimg[3]=document.getElementById("jambe");
+    partiesimg[4]=document.getElementById("pied");
+    for (let i=1;i<5;i++){
+        partiesimg[i].style.display="none";
+    }
+}
+
+function consignes(evt) {
+    alert("consignes");
+    player.focus();
+}
+
+function jouer(evt) {
+    player = document.getElementById("joueurlettreid");
+    player.focus();
+    if (player.value===""){
+
+    }
 }
